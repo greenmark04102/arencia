28f67cdf78fcfd3436a2558f150d4554fbfed0f0
greenmark04102 <lwceyb777@gmail.com>
2024-08-10 15:20:04 +0900

    page 고정

--- js/2.shop.js
+++ js/2.shop.js
@@ -149,7 +149,7 @@ $(window).resize(function () {
             $('.sideBar').stop().animate({ left: 0 }, "slow", "swing");
             $(this).css("opacity", 0);
         }));
-        $('main').on('click', function (e) {
+        $('section').on('click', function (e) {
             var container = $(".sideBar");
             if (!$(e.target).closest(container).length) {
                 $(".sideBar").stop().animate({ left: "-320" }, "slow", "swing");
